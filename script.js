d83fa50ac45feca69ce5c4e8c2f35047b9894e6d
myalisa <myalisamiro@gmail.com>
2019-12-09 20:47:16 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/*Get elements, buid functions, get event listners */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    // console.log(this.name);
+}
+/*event listneer*/
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
