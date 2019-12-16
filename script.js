dc9a1c70adf4e2488b196e7276df268ba9e3522e
Michael <michaeleburrell@gmail.com>
2019-12-16 01:39:55 -0800

    adds some working voice control

--- script.js
+++ script.js
@@ -1,94 +1,120 @@
 /* Get Our Elements */
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
 /* Build out functions */
 function togglePlay() {
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
+  const method = video.paused ? "play" : "pause";
+  video[method]();
 }
 
 function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    console.log(icon);
-    toggle.textContent = icon;
+  const icon = this.paused ? "►" : "❚ ❚";
+  console.log(icon);
+  toggle.textContent = icon;
 }
 
 function skip() {
-    video.currentTime += parseFloat(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
-    video[this.name] = this.value;
+  video[this.name] = this.value;
 }
 
 function handleProgress() {
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.flexBasis = `${percent}%`;
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 function scrub(e) {
-    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-    video.currentTime = scrubTime;
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 
 /* Hook up the event listeners */
-video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
-toggle.addEventListener('click', togglePlay);
-skipButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
 
 let mousedown = false;
-progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-progress.addEventListener('mousedown', () => mousedown = true);
-progress.addEventListener('mouseup', () => mousedown = false);
-
-
-
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", e => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
 
 /* add controller */
 
-
-
-const hero = document.querySelector('.hero');
-const text = hero.querySelector('h1');
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
 const walk = 500; // 500px
 
 function shadow(e) {
-    const { offsetWidth: width, offsetHeight: height } = hero;
-    let { offsetX: x, offsetY: y } = e;
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
 
-    if (this !== e.target) {
-        x = x + e.target.offsetLeft;
-        y = y + e.target.offsetTop;
-    }
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
 
-    const xWalk = Math.round((x / width * walk) - (walk / 2));
-    const yWalk = Math.round((y / height * walk) - (walk / 2));
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
 
-    text.style.textShadow = `
+  text.style.textShadow = `
       ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
       ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
       ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
       ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
     `;
-
 }
 
-hero.addEventListener('mousemove', shadow);
-
-
-
+hero.addEventListener("mousemove", shadow);
 
 /* new section */
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+recognition.addEventListener("result", e => {
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join("");
+
+  if (transcript.includes("play")) {
+    togglePlay();
+  }
+  if (transcript.includes("pause")) {
+    togglePlay();
+  }
+
+  // if (transcript.includes("skip")) {
+  //   skip();
+  // }
+
+  // if (transcript.includes("scrub")) {
+  //   scrub(e);
+  // }
+
+  // if (transcript.includes("progress")) {
+  //   handleProgress();
+  // }
+  console.log(transcript);
+});
+
+recognition.addEventListener("end", recognition.start);
+
+recognition.start();
